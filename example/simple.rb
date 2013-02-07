ca22863cf1e4c21b97f1a7c12b2ef3ec285b2c6c
Joshua Hawxwell <m@hawx.me>
2013-02-07 17:31:23 +0000

    Wrote example of what I want

---
+++ example/simple.rb
@@ -0,0 +1,38 @@
+require_relative '../lib/serveable/serveable'
+require 'rack'
+
+class Page
+  include Serveable::Page
+  
+  def initialize(name)
+    @name = name
+  end
+  
+  def content
+    "Hello #@name!"
+  end
+  
+  def url
+    "/#{@name.downcase}/index.html"
+  end
+end
+
+class Site
+  include Serveable::Site
+  
+  def pages
+    [
+      Page.new("Dave"),
+      Page.new("John"),
+      Page.new("Steve")
+    ]  
+  end
+end
+
+app = Rack::Builder.new do
+  use Rack::ShowExceptions
+  run Site.new
+end
+
+handler = Rack::Handler.default
+handler.run app
